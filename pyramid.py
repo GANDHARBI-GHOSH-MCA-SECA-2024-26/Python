1c1bd4f37783419f55ce84730dfc4afe88d42c47
GANDHARBI_GHOSH_MCA_A_2024-26_20_UEMK <gandharbighosh2002@gmail.com>
2025-01-18 01:26:11 +0530

    pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,9 @@
+def half_pyramid(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print("* ", end="")
+        print("")
+
+# Example: Print a half pyramid with 5 rows
+n = 5
+half_pyramid(n)
